2fe659957c62e417015f64959ce2f9ec231cf6db
Bibostik <t.fayanjuola@rgu.ac.uk>
2023-08-01 13:10:39 +0100

    Pages component

--- components/Footer.js
+++ components/Footer.js
@@ -3,27 +3,26 @@
 const Footer = () => {
     return (
       <footer className="bg-gray-900 text-white py-6">
-        <div className="container mx-auto px-4 md:px-8">
-          <div className="flex justify-between items-center">
-            <div>
-              <p className="text-lg font-bold">My Meal - Your  #1 Food App</p>
-              <p className="text-sm">Delicious food delivered to your doorstep!</p>
-            </div>
-            <nav className="space-x-4">
-              <a href="/" className="text-sm hover:text-gray-300">Home</a>
-              <a href="/about" className="text-sm hover:text-gray-300">About</a>
-              <a href="/menu" className="text-sm hover:text-gray-300">Menu</a>
-              <a href="/contact" className="text-sm hover:text-gray-300">Contact</a>
-            </nav>
+        <div className="container mx-auto px-4 md:px-8 flex flex-col items-center md:flex-row justify-between">
+          <div className="text-center md:text-left mb-4 md:mb-0">
+            <p className="text-lg font-bold">My Meal - Your #1 Food App</p>
+            <p className="text-sm">Delicious food delivered to your doorstep!</p>
           </div>
-          <div className="border-t border-gray-800 mt-4 pt-4 text-center">
-            <p className="text-sm">
-              &copy; {new Date().getFullYear()} Your Food App. All rights reserved.
-            </p>
-          </div>
+          <nav className="space-y-4 md:space-y-0 md:space-x-4">
+            <a href="/" className="text-sm hover:text-gray-300 block">Home</a>
+            <a href="/about" className="text-sm hover:text-gray-300 block">About</a>
+            <a href="/menu" className="text-sm hover:text-gray-300 block">Menu</a>
+            <a href="/contact" className="text-sm hover:text-gray-300 block">Contact</a>
+          </nav>
+        </div>
+        <div className="border-t border-gray-800 mt-4 pt-4 text-center">
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} Your Food App. All rights reserved.
+          </p>
         </div>
       </footer>
     );
   };
   
   export default Footer;
+  
