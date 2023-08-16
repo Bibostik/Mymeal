4b290b2176e12a0478cc2c8c62de2df7912abaa3
bibostik <t.fayanjuola@rgu.ac.uk>
2023-08-16 02:22:36 +0100

    Authentication for Sanity

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PaymentPage from '@/components/payment'
-import { P } from '@/mymeal-studio/dist/static/PaneItem-adf4dca2-52538969'
+
 
 export default function page() {
   return (
