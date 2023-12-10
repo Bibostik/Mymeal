be74230e0b192e98863489167fb05e56927d9e9c
Bibostik <123545774+Bibostik@users.noreply.github.com>
2023-12-10 15:49:58 +0000

    Update

--- app/layout.js
+++ app/layout.js
@@ -20,11 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen">
         <CartProvider>   
       <Navbar /> 
-        
-      {children}
+        <div className='flex-grow'>
+{children}
+        </div>
+      
       
       <Footer />    
 
--- components/cartpage.js
+++ components/cartpage.js
@@ -1,35 +1,94 @@
 "use client"
+
+
+import React from 'react';
 import { useCart } from '@/app/CartContext';
 import { useRouter } from 'next/navigation';
-import AlertModal from './alerModal'; 
-import React, { useState } from 'react';
-
-
-
+import AlertModal from './alertModal';
+import { produce } from 'immer'; 
 
 const CartPage = () => {
-  const { selectedItems } = useCart();
+  const { selectedItems, setSelectedItems } = useCart();
   const router = useRouter();
-  const [isAlertOpen, setIsAlertOpen] = useState(false);
-
-  const calculateTotal = () => {
-    return selectedItems.reduce((total, item) => total + item.price, 0);
-  };
+  const [isAlertOpen, setIsAlertOpen] = React.useState(false);
 
   const handleCheckout = () => {
     if (selectedItems.length > 0) {
-      router.push('/LoginRegister'); 
-    } else {    
-     setIsAlertOpen(true);
+      router.push('/LoginRegister');
+    } else {
+      setIsAlertOpen(true);
     }
   };
-   const handleCheckoutguest = () => {
+
+  const handleCheckoutGuest = () => {
     if (selectedItems.length > 0) {
-      router.push('/checkout'); 
-    } else {    
-     setIsAlertOpen(true);
+      router.push('/checkout');
+    } else {
+      setIsAlertOpen(true);
     }
   };
+
+  const validateItem = (item) => {
+  if (!item || typeof item !== 'object') {
+    return false;
+  }
+  if (!item._id || typeof item._id !== 'string') {
+    return false;
+  }
+  if (!item.name || typeof item.name !== 'string') {
+    return false;
+  }
+  if (!item.price || typeof item.price !== 'number' || isNaN(item.price)) {
+    return false;
+  }
+  if (!item.quantity || typeof item.quantity !== 'number' || isNaN(item.quantity)) {
+    return false;
+  }
+  return true;
+};
+
+
+
+const handleUpdateQuantity = (itemId, change) => {
+  setSelectedItems((prevItems) => {
+    return produce(prevItems, (draftItems) => {
+      const existingItemIndex = draftItems.findIndex((item) => item._id === itemId);
+
+      if (existingItemIndex !== -1) {
+        const currentQuantity = typeof draftItems[existingItemIndex].quantity === 'number'
+          ? draftItems[existingItemIndex].quantity
+          : 0;
+        const newQuantity = Math.max(0, currentQuantity + change);
+        draftItems[existingItemIndex].quantity = newQuantity;
+
+        if (newQuantity === 0) {
+          draftItems.splice(existingItemIndex, 1);
+        }
+      } else if (change > 0) {
+        // Add new item only if quantity is increased
+        draftItems.push({ _id: itemId, quantity: 1 });
+      }
+    });
+  });
+};
+
+
+  const handleClearCart = () => {
+    setSelectedItems([]);
+  };
+
+  // const calculateTotal = () => {
+  //   return selectedItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  // };
+
+const calculateTotal = () => {
+  return selectedItems.reduce((total, item) => {
+    if (typeof item.price === 'number' && typeof item.quantity === 'number') {
+      return total + item.price * item.quantity;
+    }
+    return total;
+  }, 0);
+};
 
   return (
     <div>
@@ -41,6 +100,22 @@ const CartPage = () => {
               <div key={item._id} className="border p-4 rounded-lg shadow-md mb-4">
                 <p className="font-semibold">{item.name}</p>
                 <p className="text-gray-600">Price: £{item.price}</p>
+                <div className="flex items-center space-x-4">
+                  <button
+                    onClick={() => handleUpdateQuantity(item._id, -1)}
+                    className="bg-gray-200 px-2 py-1 rounded"
+                    disabled={item.quantity === 1}
+                  >
+                    -
+                  </button>
+                  <p>{item.quantity}</p>
+                  <button
+                    onClick={() => handleUpdateQuantity(item._id, 1)}
+                    className="bg-gray-200 px-2 py-1 rounded"
+                  >
+                    +
+                  </button>
+                </div>
               </div>
             ))}
           </div>
@@ -56,16 +131,18 @@ const CartPage = () => {
               Checkout as a member
             </button>
             <button
-                    className="mt-4 w-full border border-blue-500 text-blue-500 font-semibold py-2 rounded-md hover:bg-blue-500 hover:text-white focus:outline-none"
-                    onClick={handleCheckoutguest}
+              className="mt-4 w-full border border-blue-500 text-blue-500 font-semibold py-2 rounded-md hover:bg-blue-500 hover:text-white focus:outline-none"
+              onClick={handleCheckoutGuest}
             >
-                    Checkout as a guest
+              Checkout as a guest
             </button>
-
-            <AlertModal
-                    isOpen={isAlertOpen}
-                    onRequestClose={() => setIsAlertOpen(false)}
-            />
+            <button
+              className="mt-4 w-full bg-red-500 text-white font-semibold py-2 rounded-md hover:bg-red-600 focus:outline-none"
+              onClick={handleClearCart}
+            >
+              Clear Cart
+            </button>
+            <AlertModal isOpen={isAlertOpen} onRequestClose={() => setIsAlertOpen(false)} />
           </div>
         </div>
       </div>
@@ -74,5 +151,3 @@ const CartPage = () => {
 };
 
 export default CartPage;
-
-
